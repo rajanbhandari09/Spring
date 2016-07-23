2c2572c24e1e34a0bf5afa7b0bcaf80d59528609
Rajan Bhandari <rbhand4@uic.edu>
2016-07-23 00:54:28 -0500

    Spring samples added for maven build & restful web service

---
+++ Build With Maven/src/main/java/hello/Greeter.java
@@ -0,0 +1,9 @@
+package main.java.hello;
+
+public class Greeter {
+public String sayHello(){
+	return "Hello World!";
+	
+	
+}
+}
---
+++ RestfulWebService/src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package main.java.hello;
+
+public class Greeting {
+private long id;
+private String content;
+
+Greeting(long id,String content){
+	this.id = id;
+	this.content = content;
+			
+}
+
+public long getId() {
+	return id;
+}
+
+public String getContent() {
+	return content;
+}
+	
+	
+}
